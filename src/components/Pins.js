5540f9daaf3a47e6d0f031996e6c94541d431be4
Ravishankar kushwah <ravishankark.bca2021@ssism.org>
2026-01-22 11:03:16 +0530

    update versions

--- src/components/Pins.js
+++ src/components/Pins.js
@@ -5,7 +5,7 @@ import Lazyload from "./Lazyload";
 import Pin from "./Pin"
 
 const Pins = () => {
-    const { response, isLoading, searchImage } = useContext(ImageContext);
+    const { response, isLoading,  } = useContext(ImageContext);
   return (
     <>
     <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto">
